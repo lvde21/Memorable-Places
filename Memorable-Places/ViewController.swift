daed491b5a3eb6f840c8784edf410998ca39d82d
Lala De <lvde21@live.com>
2015-05-22 23:53:49 +0800

    Annotation not showing
    Shows Map when clicked on table index
    Opens Map with correct location

---
+++ Memorable-Places/ViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ViewController.swift
+//  Memorable-Places
+//
+//  Created by Lala Vaishno De on 5/22/15.
+//  Copyright (c) 2015 Lala Vaishno De. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet var map: MKMapView!
+    
+    var manager:CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        manager = CLLocationManager();
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if activePlace == -1 {
+            manager.requestWhenInUseAuthorization()
+            manager.startUpdatingLocation()
+        }
+        else {
+            
+            let latitude = NSString(string: places[activePlace]["lat"]!).doubleValue
+            let longitude = NSString(string: places[activePlace]["lon"]!).doubleValue
+        
+            var letDelta:CLLocationDegrees = 0.01
+            var lonDelta:CLLocationDegrees = 0.01
+            var span:MKCoordinateSpan = MKCoordinateSpanMake(letDelta, lonDelta)
+            var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+            var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+            self.map.setRegion(region, animated: true)
+
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            annotation.title = places[activePlace]["name"]
+            self.map.addAnnotation(annotation)
+
+        
+        }
+        
+        
+        
+        
+    }
+
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        //println(locations)
+        
+        var userLocation:CLLocation = locations[0] as CLLocation
+        var latitude = userLocation.coordinate.latitude
+        var longitude = userLocation.coordinate.longitude
+        var letDelta:CLLocationDegrees = 0.001
+        var lonDelta:CLLocationDegrees = 0.001
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(letDelta, lonDelta)
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        self.map.setRegion(region, animated: true)
+        //self.map.showsUserLocation = true
+
+        
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        uilpgr.minimumPressDuration = 2
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    func action(gestureRecognizer: UIGestureRecognizer) {
+        
+        //ensure that only 1 long press runs once
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+        
+            var touchPoint = gestureRecognizer.locationInView(self.map)
+            var newCoordinate = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+            
+            var location:CLLocation = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            
+            var address = ""
+            
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: {(placemarks, error) -> Void in
+                //println(location)
+                
+                if error != nil {
+                    println("Reverse geocoder failed with error" + error.localizedDescription)
+                    return
+                }
+                
+                if placemarks.count > 0 {
+                    let pm = placemarks[0] as CLPlacemark
+    
+                    var subThoroughfare = pm.subThoroughfare ?? ""
+                    var thoroughfare = pm.thoroughfare ?? ""
+                    var gayfare = subThoroughfare + " " + thoroughfare
+                    var locality = pm.locality ?? ""
+                    var sublocality = pm.subLocality ?? ""
+                    var gaylocality = sublocality + " " + locality
+                    address = gayfare + " " + gaylocality
+                }
+                else {
+                    println("Problem with the data received from geocoder")
+                }
+            
+
+            
+            if (address == "" || address == " ") {
+                
+                address = "Added \(NSDate())"
+                
+            }
+                
+                places.append(["name" : address, "lat" : "\(newCoordinate.latitude)", "lon" : "\(newCoordinate.longitude)"])
+            
+            
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinate
+            annotation.title = address
+            self.map.addAnnotation(annotation)
+            
+        })
+          
+        }
+    }
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TableViewController.swift
@@ -0,0 +1,121 @@
+//
+//  TableViewController.swift
+//  Memorable-Places
+//
+//  Created by Lala Vaishno De on 5/22/15.
+//  Copyright (c) 2015 Lala Vaishno De. All rights reserved.
+//
+
+import UIKit
+
+
+//array containing dictionaries each of which are 2 strings
+var places = [Dictionary<String,String>()]
+
+var activePlace = -1
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //create an example place
+        if places.count == 1  {
+            
+            places.removeAtIndex(0)
+            
+            places.append(["name" : "Taj Mahal", "lat" : "27.175029" , "lon" : "78.042176"])
+        }
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return places.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell1", forIndexPath: indexPath) as UITableViewCell
+
+        cell.textLabel?.text = places[indexPath.row]["name"]
+
+        return cell
+    }
+    
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        
+        activePlace = indexPath.row
+        
+        return indexPath
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(true)
+        
+        self.tableView.reloadData()
+        
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
